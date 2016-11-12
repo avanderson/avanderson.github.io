846e721af0aa382e30de63a433afc480959eae7a
ameliacurrie1 <amelia_currie@msn.com>
2016-11-12 19:20:51 +0000

    updates
    having a few problems making the contact info go horizontal and in the
    area just to the right of the email sign up box.

--- jquery.js
+++ jquery.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
 
-$(".bxslider").bxSlider();
+$(".bxslider").bxSlider({
+	auto: true,
+	autoControls: false,
+	controls: false,
+	pause: 4000,
+	infiniteLoop: true,
+	mode: 'horizontal',
+	autoDirection: 'next',
+	responsive: true,
+	autoDelay: 0,
+	randomStart: false,
+	pager: false,
+	moceSlideQty: 1,
+	useCSS: false,	
+	adaptiveHeight: true
+});
 
 $(function() {
     $("#map").googleMap({
@@ -8,7 +23,11 @@ $(function() {
       coords: [51.5398305,-0.1544718,17], // Map center (optional)
       type: "ROADMAP" // Map type (optional)
     });
-  })
+		})
 
+$("#contactUs").click(function () {
+	$("dialog").dialog("open");
+	return false;
+});
 
 });
